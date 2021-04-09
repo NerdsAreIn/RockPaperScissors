9d0e201cf4037621cdf3a67568fafeb891211543
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-04-09 13:46:46 +0100

    More work on styles. Nearing completion.

--- rockPaperScissors_UI.js
+++ rockPaperScissors_UI.js
@@ -5,6 +5,12 @@ function computerPlay() {
     return computerSelection;
 }
 
+const resultsBox = document.querySelector("div");
+//resultsBox.textContent = "";
+const resultsPara1 = document.getElementById("p1");
+const resultsPara2 = document.querySelector("#p2");
+//resultsPara1.textContent = "";
+//resultsPara2.textContent = "";
 const buttons = Array.from(document.getElementsByTagName("button"));
 buttons.forEach((button) => {
       button.addEventListener("click", function(event) {
@@ -12,25 +18,22 @@ buttons.forEach((button) => {
     singleRound(event);    
       });
 });
-
-const resultsBox = document.querySelector("div");
-resultsBox.textContent = "";
 let playerScore = 0;
 let computerScore = 0;
 
 function tallyScore() {
     if (/win/i.test(result)) {
         playerScore += 1;
-        resultsBox.textContent += (`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
+        resultsPara2.textContent = (`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         console.log(`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         }
     else if (/lose/i.test(result)) {
         computerScore += 1;
-        resultsBox.textContent += (`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
+        resultsPara2.textContent = (`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         console.log(`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         }
     else {
-        resultsBox.textContent += (`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
+        resultsPara2.textContent = (`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         console.log(`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         }
     }
@@ -44,35 +47,36 @@ function singleRound(event, computerSelection) {
     }
     if (playerSelection.getAttribute("id") == "rock" && computerSelection == "Scissors") {
         result = "You win! Rock blunts scissors!" + "\n";
-        resultsBox.textContent = result;
+        resultsPara1.textContent = result;
     }
     else if (playerSelection.getAttribute("id") == "paper" && computerSelection == "Rock") {
         result = "You win! Paper encloses rock!";
-        resultsBox.textContent = result;        
+        resultsPara1.textContent = result;        
     } 
     else if (playerSelection.getAttribute("id") == "scissors" && computerSelection == "Paper") {
         result = "You win! Scissors cut paper!";
-        resultsBox.textContent = result;
+        resultsPara1.textContent = result;
     }
     else if (playerSelection.getAttribute("id") == "rock" && computerSelection == "Paper") {
         result = "You lose. Paper encloses rock."
-        resultsBox.textContent = result;
+        resultsPara1.textContent = result;
     }
     else if (playerSelection.getAttribute("id") == "paper" && computerSelection == "Scissors") {
         result = "You lose. Scissors cut paper.";
-        resultsBox.textContent = result;
+        resultsPara1.textContent = result;
     }
     else if (playerSelection.getAttribute("id") == "scissors" && computerSelection == "Rock") {
         result = "You lose. Rock blunts scissors."
-        resultsBox.textContent = result;
+        resultsPara1.textContent = result;
     } 
     else {      
         result = "It's a draw.";
-        resultsBox.textContent = result;
+        resultsPara1.textContent = result;
     }
     if (playerScore == 5) {
     displayFinalScore();
-    resultsBox.textContent = "";
+    resultsPara1.textContent = "";
+    resultsPara2.textContent = "";
     resultsBox.textContent = "That's the end  of the game. Refresh the page to start a new game.";
     }
     else tallyScore();
@@ -80,15 +84,15 @@ function singleRound(event, computerSelection) {
      
     function displayFinalScore() {
         if (playerScore > computerScore) {
-        resultsBox.textContent += (`You won! Final score: \n Your score: ${playerScore}. Computer's score: ${computerScore}.`);
+        resultsPara2.textContent = (`You won! Final score: \n Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         console.log(`You won! Final score: \n Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         }
         else if (computerScore > playerScore) {
-        resultsBox.textContent += (`You lost. Final score: \n Your score: ${playerScore}. Computer's score: ${computerScore}.`);
+        resultsPara2.textContent = (`You lost. Final score: \n Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         console.log(`You lost. Final score: \n Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         }
         else {
-        resultsBox.textContent += (`It's a draw. Final score: \n Your score: ${playerScore}. Computer's score: ${computerScore}.`);
+        resultsPara2.textContent = (`It's a draw. Final score: \n Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         console.log(`It's a draw. Final score: \n Your score: ${playerScore}. Computer's score: ${computerScore}.`);
         }
     }
